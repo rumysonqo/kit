0af09ce33fcae9ad504d027d61753538697e11e2
abel laurente <abellaurente@hotmail.com>
2022-02-17 13:24:00 -0500

    uno

--- src/base.js
+++ src/base.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'abel',
-    password: 'axl',
+    password: 'Indira2475',
     database: 'siga'
 });
 
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const con = require('../base');
 router.get('/', (req, res) => {
-    con.query('select * from kit21', (err, rows, fields) => {
+    con.query('select * from kit', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 
 router.get('/kit_por_programa/:prg', (req, res) => {
     const { prg } = req.params;
-    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, meta, sub_finalidad, clasificador from kit21 where cod_programa=? order by cod_meta, sub_finalidad', [prg], (err, rows, fields) => {
+    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, meta, sub_finalidad, clasificador from kit where cod_programa=? order by cod_meta, sub_finalidad', [prg], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -25,7 +25,7 @@ router.get('/kit_por_programa/:prg', (req, res) => {
 
 
 router.get('/programas', (req, res) => {
-    con.query('select distinct(cod_programa) as cod_programa, programa from kit21 order by cod_programa', (err, rows, fields) => {
+    con.query('select distinct(cod_programa) as cod_programa, programa from kit order by cod_programa', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -36,7 +36,7 @@ router.get('/programas', (req, res) => {
 
 router.get('/metas/:prg', (req, res) => {
     const { prg } = req.params;
-    con.query('select distinct(cod_meta) as cod_meta, meta from kit21 where cod_programa=? order by cod_meta', [prg], (err, rows, fields) => {
+    con.query('select distinct(cod_meta) as cod_meta, meta from kit where cod_programa=? order by cod_meta', [prg], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -46,7 +46,7 @@ router.get('/metas/:prg', (req, res) => {
 });
 
 router.get('/metas', (req, res) => {
-    con.query('select distinct(cod_meta) as cod_meta, meta from kit21 order by cod_meta', (err, rows, fields) => {
+    con.query('select distinct(cod_meta) as cod_meta, meta from kit order by cod_meta', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -58,7 +58,7 @@ router.get('/metas', (req, res) => {
 
 router.get('/clasif/:met', (req, res) => {
     const { met } = req.params;
-    con.query('select distinct(cod_clasificador), clasificador from kit21 where cod_meta=?', [met], (err, rows, fields) => {
+    con.query('select distinct(cod_clasificador), clasificador from kit where cod_meta=?', [met], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -69,7 +69,7 @@ router.get('/clasif/:met', (req, res) => {
 
 router.get('/tareas/:prg/:met', (req, res) => {
     const { prg, met } = req.params;
-    con.query('select distinct(cod_subfin) as cod_tarea, sub_finalidad as tarea from kit21 where cod_programa=? and cod_meta=? order by cod_subfin', [prg, met], (err, rows, fields) => {
+    con.query('select distinct(cod_subfin) as cod_tarea, sub_finalidad as tarea from kit where cod_programa=? and cod_meta=? order by cod_subfin', [prg, met], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -81,7 +81,7 @@ router.get('/tareas/:prg/:met', (req, res) => {
 
 router.get('/kit_por_meta/:prg/:met', (req, res) => {
     const { prg, met } = req.params;
-    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, sub_finalidad, clasificador from kit21 where cod_programa=? and cod_meta=? order by sub_finalidad, nom_item_fam', [prg, met], (err, rows, fields) => {
+    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, sub_finalidad, clasificador from kit where cod_programa=? and cod_meta=? order by sub_finalidad, nom_item_fam', [prg, met], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -92,7 +92,7 @@ router.get('/kit_por_meta/:prg/:met', (req, res) => {
 
 router.get('/kit_clasif/:clf', (req, res) => {
     const { clf } = req.params;
-    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, sub_finalidad, clasificador from kit21 where cod_clasificador=? order by sub_finalidad, nom_item_fam', [clf], (err, rows, fields) => {
+    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, sub_finalidad, clasificador from kit where cod_clasificador=? order by sub_finalidad, nom_item_fam', [clf], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -104,7 +104,7 @@ router.get('/kit_clasif/:clf', (req, res) => {
 
 router.get('/kit_por_tarea/:prg/:met/:tar', (req, res) => {
     const { prg, met, tar } = req.params;
-    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, sub_finalidad, clasificador from kit21 where cod_programa=? and cod_meta=? and cod_subfin=? order by sub_finalidad, nom_item_fam', [prg, met, tar], (err, rows, fields) => {
+    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, sub_finalidad, clasificador from kit where cod_programa=? and cod_meta=? and cod_subfin=? order by sub_finalidad, nom_item_fam', [prg, met, tar], (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
