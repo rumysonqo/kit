9391dcfc94c6128c93a8b848594f5a3d18566c11
abel laurente <abellaurente@hotmail.com>
2022-02-18 11:53:26 -0500

    aaa

--- src/base.js
+++ src/base.js
@@ -1,11 +1,21 @@
 const mysql = require('mysql2');
 
+const con = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "axl",
+    database: process.env.MYSQL_DATABASE || "siga",
+  });
+
+/*
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'abel',
     password: 'Indira2475',
     database: 'siga'
 });
+*/
 
 con.connect(function(err) {
     if (err) {
